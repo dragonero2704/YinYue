eb0d0ce50e17191b6c2ea07eee8f182960bc2304
Roberto Rudi <60794906+dragonero2704@users.noreply.github.com>
2021-09-09 21:37:06 +0000

    bugfix on bulkdelete

--- commands/bulkdelete/bulkdelete.js
+++ commands/bulkdelete/bulkdelete.js
@@ -3,6 +3,10 @@ module.exports = {
   name: 'bulkdelete',
   aliases: ['clean'],
   run: (msg, args, bot, Discord) =>{
-    msg.channel.bulkDelete(parseInt(args[1]))
+    try{
+      msg.channel.bulkDelete(parseInt(args[1]))
+    }catch(error){
+      console.log(error)
+    } 
   }
 }
