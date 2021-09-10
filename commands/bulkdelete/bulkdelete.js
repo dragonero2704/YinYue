d6ccccad9b7166a6e9008e1a4fa7d3ab8c639f5f
Roberto Rudi <roberto.rudi04@gmail.com>
2021-09-10 20:07:11 +0200

    fixed bulkdelete

--- commands/bulkdelete/bulkdelete.js
+++ commands/bulkdelete/bulkdelete.js
@@ -1,12 +1,20 @@
-
 module.exports = {
-  name: 'bulkdelete',
-  aliases: ['clean'],
-  run: (msg, args, bot, Discord) =>{
-    try{
-      msg.channel.bulkDelete(parseInt(args[1]))
-    }catch(error){
-      console.log(error)
-    } 
-  }
+    name: 'bulkdelete',
+    aliases: ['clean'],
+    run: (msg, args, bot, Discord) => {
+
+        if (!args[1]) {
+            let embed = require('../../embed')(msg.guild)
+            embed.addField('Inserisci un numero!')
+            msg.channel.send({ embeds: [embed] }).then(msg => {
+                setTimeout(() => msg.delete(), 10000)
+            })
+        }
+
+        try {
+            msg.channel.bulkDelete(parseInt(args[1]))
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
