c6e062eb9f7c5095c91844666696f85573313950
Roberto Rudi <roberto.rudi04@gmail.com>
2022-02-07 15:40:19 +0100

    updated ping.js

--- commands/test_commands/ping.js
+++ commands/test_commands/ping.js
@@ -6,14 +6,14 @@ module.exports = {
     run(msg, args, bot) {
         let embed = require('../../embed')(msg.guild)
         embed.setTitle(`Ping: ${bot.ws.ping}ms`)
-        msg.channel.send({ embeds: [embed] })
+        msg.reply({ embeds: [embed], ephemeral: true })
     },
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Calcola il ping'),
-    async execute(inter, bot, Discord) {
+    async execute(inter, bot) {
         let embed = require('../../embed')(inter.guild)
         embed.setTitle(`Ping: ${bot.ws.ping}ms`)
-        await inter.reply({ embeds: [embed] })
+        await inter.reply({ embeds: [embed], ephemeral: true })
     }
 }
