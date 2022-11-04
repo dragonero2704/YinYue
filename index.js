44a5fdfd44c907101eed8896f53e5cf80ad1bb20
dragonero2704 <roberto.rudi04@gmail.com>
2022-11-04 11:32:44 +0100

    index changes

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ const { Client, GatewayIntentBits, Collection } = require('discord.js')
 
 const { config } = require("dotenv")
 
-const { readdirSync } = require('fs')
-const fs = require('fs')
-// const keepAlive = require('./server/server')
-
-
-
+const { readdirSync, appendFile } = require('fs')
 
 const bot = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent],
@@ -27,9 +22,8 @@ function getTimeStamp() {
 
 function getLogName(){
     let date = new Date()
-    return `${date.getDay()}_${date.getMonth()}_${date.getFullYear()}.log`
+    return `${date.getFullYear()}_${date.getMonth()}_${date.getDay()}.log`
 }
-
 
 let handler_path = __dirname + '/handlers'
 readdirSync(handler_path).forEach((handler) => {
@@ -40,13 +34,11 @@ config({
     path: __dirname + '/.env'
 })
 
-// keepAlive()
 console.log = function () {
     // origLog.call(console, getTimeStamp())
     process.stdout.write(getTimeStamp() + ': ')
-    
     //write to file
-    fs.appendFile(`./logs/${getLogName()}`, `${getTimeStamp()}: ${arguments[0]}\n`, (err)=>{
+    appendFile(`./logs/${getLogName()}`, `${getTimeStamp()}: ${arguments[0]}\n`, (err)=>{
         if(err){
             origLog(err)
         }else{
