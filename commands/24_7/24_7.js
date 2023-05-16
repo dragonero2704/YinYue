bacc39c53356f32be22c981a9d65f4cda329477a
dragonero2704 <roberto.rudi04@gmail.com>
2023-05-16 09:00:34 +0200

    removed useless import

--- commands/24_7/24_7.js
+++ commands/24_7/24_7.js
@@ -1,6 +1,5 @@
 const { globalQueue } = require('../../misc/globals')
 
-const { ServerQueue, check } = require('./serverQueue');
 const { SlashCommandBuilder, basename } = require('discord.js');
 
 const lang = require(`./languages/${basename(__filename).split('.')[0]}.json`)
